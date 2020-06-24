7eb5f23b85b355ef4d7246f433b69db22e0a2d7e
Aniruddha Achar B P <anacharb@microsoft.com>
2020-06-24 22:56:08 +0530

    Added crash analysis to the analysis engine and the crash view to the report view engine.

--- src/dump-diagnostic/AnalysisEngine/Engines/CrashAnalysis.cs
+++ src/dump-diagnostic/AnalysisEngine/Engines/CrashAnalysis.cs
@@ -1,22 +1,26 @@
 ﻿using Microsoft.Diagnostics.Runtime;
 using ReportEngine;
+using ReportEngine.Helpers;
+using ReportEngine.ReportView;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace AnalysisEngine.Engines
 {
     public sealed class CrashAnalysis : BaseAnalysisEngine, IAnalysisEngine
     {
-        private string dumpPath { get; set; }
-        private IReportEngine reportEngine { get; set; }
+        private string DumpPath { get; set; }
 
-        public CrashAnalysis(string dumpPath, IReportEngine reportEngine, bool verbose = false) :
+        CrashReportView viewModel;
+
+        public CrashAnalysis(string dumpPath, bool verbose = false) :
             base(verbose)
         {
-            this.dumpPath = dumpPath;
-            this.reportEngine = reportEngine;
+            this.DumpPath = dumpPath;
             try
             {
                 CreateDataTarget(dumpPath);
@@ -39,14 +43,105 @@ namespace AnalysisEngine.Engines
         }
         public void GenerateReport(IReportEngine reportEngine)
         {
-            throw new NotImplementedException();
+            reportEngine.GenerateReport(viewModel, null);
         }
 
         public void RunAnalysis()
         {
-            throw new NotImplementedException();
+            viewModel = new CrashReportView
+            {
+                ThreadDetails = GetThreadDetails(),
+
+            };
         }
 
-        
+        private IEnumerable<AnalysisThread> GetThreadDetails()
+        {
+            foreach (var thread in Runtime.Threads)
+            {
+                var at = new AnalysisThread(thread)
+                {
+                    Address = thread.Address,
+                    LockCount = thread.LockCount,
+                    ManagedThreadId = thread.ManagedThreadId,
+                    OSThreadId = thread.OSThreadId,
+                    StackBase = thread.StackBase,
+                    StackLimit = thread.StackBase
+                };
+                at.SetApartment();
+                at.SetException();
+                at.SetFinalizer();
+                at.SetGCMode();
+                yield return at;
+            }
+        }
+
+        private string GetExceptionCallStack(ClrException exception, bool innerException = false)
+        {
+            StringBuilder @string = new StringBuilder();
+            string innerEx = "";
+            if(innerException)
+            {
+                @string.Append("Inner exception:\n");
+            }
+            @string.Append("Exception details:\n");
+            @string.Append($"{exception.Type.Name}\n");
+            @string.Append("Exception Message:\n");
+            @string.Append($"{exception.Message}\n\n");
+            @string.Append("Exception stack");
+            @string.Append(GetExceptionStackFrames(exception.StackTrace));
+            if (exception.Inner != null)
+            {
+                innerEx = GetExceptionCallStack(exception.Inner, true);
+            }
+            @string.Append(innerEx);
+            return @string.ToString();
+        }
+
+        private IEnumerable<(int ManagedThreadId, uint OSThreadId, string stackFrames, string exceptionDetails)> GetThreadsWithException()
+        {
+            var threadsWithException = Runtime.Threads.Where(t => t.CurrentException != null);
+
+            foreach(var t in threadsWithException)
+            {
+                yield return (t.ManagedThreadId, t.OSThreadId, GetStackFrames(t.EnumerateStackTrace()), GetExceptionCallStack(t.CurrentException));
+            }
+        }
+
+        private string GetExceptionStackFrames(ImmutableArray<ClrStackFrame>  stackTrace)
+        {
+            StringBuilder @string = new StringBuilder();
+            foreach (var frame in stackTrace)
+            {
+                if (frame.Kind.ToString("G") == "Internal")
+                {
+                    @string.Append("----Internal---\n");
+                }
+                else
+                {
+                    @string.Append($"{frame.Method}\n");
+                }
+            }
+
+            return @string.ToString();
+        }
+        private string GetStackFrames(IEnumerable<ClrStackFrame> stackTrace)
+        {
+            StringBuilder @string = new StringBuilder();
+            foreach (var frame in stackTrace)
+            {
+                if (frame.Kind.ToString("G") == "Internal")
+                {
+                    @string.Append("----Internal---\n");
+                }
+                else
+                {
+                    @string.Append($"{frame.Method}\n");
+                }
+            }
+
+            return @string.ToString();
+        }
+
     }
 }
---
+++ src/dump-diagnostic/ReportEngine/ReportView/CrashReportView.cs
@@ -0,0 +1,14 @@
+﻿using ReportEngine.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReportEngine.ReportView
+{
+    public class CrashReportView : BaseReportView
+    {
+        public IEnumerable<AnalysisThread> ThreadDetails { get; set; }
+        public IEnumerable<(int ManagedThreadId, uint OSThreadId, string stackFrames, string exceptionDetails)> ThreadsWithExceptions 
+        { get; set; }
+    }
+}
