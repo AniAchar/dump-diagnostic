b3c96db5f4df17bccc4fa53a65919864f13c3c71
Aniruddha Achar B P <anacharb@microsoft.com>
2020-06-26 22:07:49 +0530

    Added crash report rendering rules.

--- src/dump-diagnostic/ReportEngine/ConsoleEngine.cs
+++ src/dump-diagnostic/ReportEngine/ConsoleEngine.cs
@@ -28,6 +28,11 @@ namespace ReportEngine
             {
                 RenderMemoryReport((MemoryReportView)viewModel);
             }
+            if(_reportType.ToLower() == "crash")
+            {
+                RenderCrashReport((CrashReportView)viewModel);
+            }
+
         }
 
         private void RenderMemoryReport(MemoryReportView viewModel)
@@ -67,7 +72,35 @@ namespace ReportEngine
             screen.Render(region);
         }
 
+        private void RenderCrashReport(CrashReportView viewModel)
+        {
+            var region = new Region(0,
+                                    0,
+                                    1080,
+                                    10800,
+                                    true);
 
+
+
+            var console = _invocationContext.Console;
+
+            if (console is ITerminal terminal)
+            {
+                terminal.Clear();
+            }
+
+            console.Append(new ContentView("Dump details"));
+            console.Append(new SplitterView());
+            console.Append(new DumpDetailsView(viewModel));
+            StackLayoutView stackLayoutView2 = new StackLayoutView
+            {
+                new TemplateStackView("Thread details", new ThreadsDetailView(viewModel)),
+                new TemplateStackView("Threads with exceptions", new StackFramesStackView(viewModel)),
+                new SplitterView()
+            };
+            var screen = new ScreenView(_consoleRenderer, _invocationContext.Console) { Child = stackLayoutView2 };
+            screen.Render(region);
+        }
 
     }
 }
