70d8e7f259693761a755622b5e0895551c425787
Aniruddha Achar B P <anacharb@microsoft.com>
2020-07-07 01:18:28 +0530

    Added dump file path to the reporting.

--- src/dump-diagnostic/ReportEngine/ReportTemplates/ConsoleTemplates/DumpDetailsView.cs
+++ src/dump-diagnostic/ReportEngine/ReportTemplates/ConsoleTemplates/DumpDetailsView.cs
@@ -11,7 +11,7 @@ namespace ReportEngine.ReportTemplates.ConsoleTemplates
         public DumpDetailsView(BaseReportView reportView)
         {
             SetColumns(ColumnDefinition.Star(0.3), ColumnDefinition.Star(1));
-            SetRows(RowDefinition.Star(1), RowDefinition.Star(1), RowDefinition.Star(1), RowDefinition.Star(1), RowDefinition.Star(1));
+            SetRows(RowDefinition.Star(1), RowDefinition.Star(1), RowDefinition.Star(1), RowDefinition.Star(1), RowDefinition.Star(1), RowDefinition.Star(1));
             SetChild(new ContentView("Main Module Name"), 0, 0);
             SetChild(new ContentView($"{reportView.MainModuleName}"), 1, 0);
             SetChild(new ContentView("Clr Version"), 0, 1);
@@ -20,6 +20,8 @@ namespace ReportEngine.ReportTemplates.ConsoleTemplates
             SetChild(new ContentView($"{reportView.DotnetFlavor}"), 1, 2);
             SetChild(new ContentView("GC Mode"), 0, 3);
             SetChild(new ContentView($"{reportView.GCMode}"), 1, 3);
+            SetChild(new ContentView("Dump file analyzed"), 0, 4);
+            SetChild(new ContentView($"{reportView.DumpPath}"), 1, 4);
         }
 
     }
