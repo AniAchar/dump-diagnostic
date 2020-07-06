656a177ffcfec9612d29ed077b25d0bbb44a2b5d
Aniruddha Achar B P <anacharb@microsoft.com>
2020-07-07 02:11:10 +0530

    Moved away from DragonFruit and using CommandLine for more options

--- src/dump-diagnostic/CLI/src/Program.cs
+++ src/dump-diagnostic/CLI/src/Program.cs
@@ -6,111 +6,36 @@ using AnalysisEngine;
 using Microsoft.Diagnostics.Runtime;
 using System.CommandLine.Invocation;
 using System.CommandLine;
+using System.CommandLine.Rendering;
+using System.CommandLine.IO;
+using System.Threading.Tasks;
+using ReportEngine.ReportRenderer;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
 
 namespace DumpDiagnostic
 {
     class Program
     {
-        static void Main(InvocationContext invocationContext, FileInfo dumpPath, DiagnosisType diagnose, ReportTypes reportType, bool verbose)
+
+
+        public static Task<int> Main(string[] args)
         {
-            CheckArguments(dumpPath, diagnose, reportType);
-            bool initFailed = false;
-
-            IReportEngine reportEngine;
-            IAnalysisEngine analysisEngine;
-
-            switch (reportType)
-            {
-                case ReportTypes.console:
-                    {
-                        reportEngine = new ConsoleEngine(diagnose.ToString("G"), invocationContext.Console);
-                        break;
-                    }
-                default:
-                    reportEngine = null;
-                    break;
-
-            }
-
-            switch (diagnose)
-            {
-                case DiagnosisType.memory:
-                    {
-                        try
-                        {
-                            analysisEngine = new MemoryAnalysis(dumpPath.FullName, verbose);
-                            
-                            RunAnalysis(analysisEngine);
-                            GenerateReport(analysisEngine);
-                        }
-                        catch(Exception e)when(e is FileNotFoundException || e is ArchitectureNotMatchException)
-                        {
-                            initFailed = true;
-                            Console.WriteLine(e.Message);
-                        }
-                        catch(ClrDiagnosticsException e)
-                        {
-                            initFailed = true;
-                            Console.WriteLine($"Failed to create the runtime. Use the --verbose switch to get more information\n {e.Message}");
-                        }
-                        break;
-                    }
-                case DiagnosisType.crash:
-                    {
-                        try
-                        {
-                            analysisEngine = new CrashAnalysis(dumpPath.FullName, verbose);
-
-                            RunAnalysis(analysisEngine);
-                            GenerateReport(analysisEngine);
-                        }
-                        catch (Exception e) when (e is FileNotFoundException || e is ArchitectureNotMatchException)
-                        {
-                            initFailed = true;
-                            Console.WriteLine(e.Message);
-                        }
-                        catch (ClrDiagnosticsException e)
-                        {
-                            initFailed = true;
-                            Console.WriteLine($"Failed to create the runtime. Use the --verbose switch to get more information\n {e.Message}");
-                        }
-                        break;
-                    }
-                default:
-                    {
-                        break;
-                    }
-            }
-
-            void RunAnalysis(IAnalysisEngine analysisEngine)
-            {
-                if(!initFailed)
-                {
-                    analysisEngine.RunAnalysis();
-                }
-                else
-                {
-                    Console.WriteLine("Initialization failed. Cannot run analysis");
-                }
-            }
-            void GenerateReport(IAnalysisEngine analysisEngine)
-            {
-                if (!initFailed)
-                {
-                    analysisEngine.GenerateReport(reportEngine);
-                }
-                else
-                {
-                    Console.WriteLine("Initialization failed. Cannot run analysis");
-                }
-            }
+            var parser = new CommandLineBuilder()
+                .AddCommand(MemoryDiagCommand())
+                .AddCommand(CrashDiagCommand())
+                .UseAnsiTerminalWhenAvailable()
+                .UseDefaults()
+                .Build();
+            return parser.InvokeAsync(args);
 
         }
+
 
         public enum ReportTypes
         {
             //TODO: Implement the HTML rendering options.
-            console,       
+            console,
         }
         public enum DiagnosisType
         {
@@ -120,13 +45,105 @@ namespace DumpDiagnostic
 
         private static void CheckArguments(FileInfo dumpPath, DiagnosisType diagnose, ReportTypes reportType)
         {
-            if(dumpPath== null || diagnose == null || reportType == null)
+            if (dumpPath == null)
             {
                 Console.WriteLine("Missing argument(s). Please use dump-diagnostic -h to look at all the required arguments");
                 Environment.Exit(-1);
             }
         }
 
+        private static Option DumpPathOption() =>
+            new Option(
+                aliases: new[] { "-dp", "--dump-path" },
+                description: "The absolute file path to the dump file that needs to be diagnosed.")
+            {
+                Argument = new Argument<FileInfo>(name: "dump path")
+            };
 
+        private static Option VerboseOptions() =>
+            new Option(
+                aliases: new[] { "-v", "--verbose" },
+                description: "Get verbose logs to help debug the app.")
+            {
+                Argument = new Argument<bool>(name: "verbose")
+            };
+
+        private static Option ReportOptions() =>
+            new Option(
+                aliases: new[] { "-r", "--report" },
+                description: "Generates a report of a specific format.")
+            {
+                Argument = new Argument<ReportTypes>(name: "report")
+            };
+
+        private static Command MemoryDiagCommand()
+        {
+            var cmd = new Command(
+                    name: "memory",
+                    description: "Runs a memory diagnosis on the dumps and shows dotnet memory profile of the dump.")
+            {
+                // Arguments and Options
+                DumpPathOption(),
+                VerboseOptions(),
+                ReportOptions()
+            };
+            cmd.Handler = CommandHandler.Create((FileInfo dumpPath, ReportTypes reportTypes, bool verbose) =>
+            {
+                try
+                {
+                    IAnalysisEngine analysisEngine;
+                    analysisEngine = new MemoryAnalysis(dumpPath.FullName, verbose);
+
+                    analysisEngine.RunAnalysis();
+                    var memoryReportRenderer = new MemoryReportRenderer();
+                    analysisEngine.GenerateReport(new ConsoleEngine(memoryReportRenderer));
+                }
+                catch (Exception e) when (e is FileNotFoundException || e is ArchitectureNotMatchException)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (ClrDiagnosticsException e)
+                {
+                    Console.WriteLine($"Failed to create the runtime. Use the --verbose switch to get more information\n {e.Message}");
+                }
+            }
+                    );
+            return cmd;
+        }
+
+        private static Command CrashDiagCommand()
+        {
+            var cmd = new Command(
+                   name: "crash",
+                   description: "Runs a crash analysis on the dumps and shows dotnet exception callstack.")
+            {
+                // Arguments and Options
+                DumpPathOption(),
+                VerboseOptions(),
+                ReportOptions()
+            };
+            cmd.Handler = CommandHandler.Create((FileInfo dumpPath, ReportTypes reportTypes, bool verbose) =>
+            {
+                try
+                {
+                    IAnalysisEngine analysisEngine;
+                    analysisEngine = new CrashAnalysis(dumpPath.FullName, verbose);
+
+                    analysisEngine.RunAnalysis();
+                    var reportRenderer = new CrashReportRenderer();
+                    analysisEngine.GenerateReport(new ConsoleEngine(reportRenderer));
+                }
+                catch (Exception e) when (e is FileNotFoundException || e is ArchitectureNotMatchException)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (ClrDiagnosticsException e)
+                {
+                    Console.WriteLine($"Failed to create the runtime. Use the --verbose switch to get more information\n {e.Message}");
+                }
+            }
+                    );
+            return cmd;
+        }
     }
 }
